b2d3d10af59ac3097c7e1c6c74f08da784caccc1
Jobbel <jckieser@gmx.de>
2023-03-13 02:30:40 +0100

    added fsck to startup script

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,12 @@ DEVNAME=/dev/sda1
 MOUNT_POINT=/mnt/storage
 ID_FS_TYPE=vfat
 
+# umount if mounted, required to safely run fsck
+umount -v $DEVNAME
+
+# check drive and autofix
+fsck -p $DEVNAME
+
 # Mount USB drive on startup
 mkdir -p $MOUNT_POINT
 mount -v -o rw $DEVNAME $MOUNT_POINT
