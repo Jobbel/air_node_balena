48b56b18baf8593695c3de31e00cc33fd77f85e3
Jobbel <jckieser@gmx.de>
2023-10-20 03:11:03 +0200

    new start script which chooses between sda and sdb for usb drive

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
 # USB stick variables
-DEVNAME=/dev/sda1
 MOUNT_POINT=/mnt/storage
-ID_FS_TYPE=vfat
 
-# umount if mounted, required to safely run fsck
-umount -v $DEVNAME
+# Ensure the mount point exists
+mkdir -p $MOUNT_POINT
 
-# check drive and autofix
+# Check if /dev/sda exists
+if [ -e /dev/sda1 ]; then
+    DEVNAME=/dev/sda1
+else
+    DEVNAME=/dev/sdb1
+fi
+
+# Unmount the device if it's already mounted
+if mount | grep -q "$DEVNAME on $MOUNT_POINT"; then
+    umount -v $MOUNT_POINT
+fi
+
+# Attempt to repair the USB drive with fsck (for VFAT)
 fsck -p $DEVNAME
 
-# Mount USB drive on startup
-mkdir -p $MOUNT_POINT
+# Check the exit status of fsck
+if [ $? -eq 0 ]; then
+    echo "USB drive file system restored successfully."
+else
+    echo "USB drive file system repair failed. You may need to manually repair the device."
+fi
+
+# Mount USB drive
 mount -v -o rw $DEVNAME $MOUNT_POINT
 
 # Start main program
